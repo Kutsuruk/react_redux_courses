f10c4774de465a332d15f738190e758d7d83e6d5
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-04 20:56:28 +0200

    Learn about State and did some practise exercise.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Lesson from './components_02/Lesson'
-import CounterButton from './State_03/Lesson'
+import CounterButton from './03_state/Lesson'
+import Lesson from './02_components/Lesson';
+
 
 React.createElement('div', null, 'Simple div with text')
 React.createElement('input', {className: 'just-class'})
 
 ReactDOM.render(
-<CounterButton />
-,document.getElementById('root'));
+<CounterButton />,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
